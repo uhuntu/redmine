90cc7a16c109f4c3e2bb2fc570b1ff0cb6e50845
Hunt Lin <hunt.lin@tes-tec.com>
2022-11-25 14:44:51 +0800

    some

---
+++ install.sh
@@ -0,0 +1,5 @@
+bundle install
+bundle exec rake redmine:plugins NAME=easy_mindmup RAILS_ENV=production
+bundle exec rake redmine:plugins NAME=easy_wbs RAILS_ENV=production
+bundle exec rake db:migrate RAILS_ENV=production
+bundle exec rake redmine:plugins:migrate RAILS_ENV=production
