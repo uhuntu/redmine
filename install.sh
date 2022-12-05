da45e80522d380d05024feeacbf8075a8be9b95a
Hunt Lin <hunt.lin@tes-tec.com>
2022-12-05 09:49:19 +0800

    change

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 bundle install
 
 bundle exec rake zeitwerk:check --trace
-
 bundle exec rake db:migrate RAILS_ENV=production
 
 bundle exec rake redmine:plugins NAME=easy_wbs RAILS_ENV=production
 bundle exec rake redmine:plugins NAME=000_redmine_x_ux_upgrade RAILS_ENV=production
 
-bundle exec rake redmine:plugins:migrate RAILS_ENV=production
+bundle exec rake redmine:plugins:migrate NAME=easy_wbs RAILS_ENV=production
+bundle exec rake redmine:plugins:migrate NAME=000_redmine_x_ux_upgrade RAILS_ENV=production
+
+bundle exec rake db:migrate RAILS_ENV=production
+bundle exec rake zeitwerk:check --trace
+
+bundle install
