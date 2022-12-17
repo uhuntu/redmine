7ac7528264f5c5be3f40217ccf7fa625ef1ae970
Hunt Lin <hunt.lin@tes-tec.com>
2022-12-17 18:04:14 +0800

    again

--- production.sh
+++ production.sh
@@ -1,2 +1,4 @@
+bundle exec rake db:drop db:create RAILS_ENV=production
 bundle exec rake db:migrate RAILS_ENV=production
+bundle exec rake redmine:plugins RAILS_ENV=production
 bundle exec rake redmine:load_default_data RAILS_ENV=production REDMINE_LANG=en
