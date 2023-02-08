08248fa0dc76c7b3b4d04eff3f6597364b90c7c1
Hunt Lin <hunt.lin@tes-tec.com>
2023-02-08 18:14:10 +0800

    backup

--- remove.sh
+++ remove.sh
@@ -1,5 +1,4 @@
 rm -rfv /opt/redmine/redmine-5.0.3/public/themes/redminex_theme
-
 rm -rfv /opt/redmine/redmine-5.0.3/public/plugin_assets
 
-#bundle exec rake redmine:plugins NAME=000_redmine_x_ux_upgrade VERSION=0 RAILS_ENV=production
+mv plugins backup
