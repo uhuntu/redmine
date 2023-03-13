3ee4669ad55d0fb4b2e6ee55b2af8d3477fa7702
Hunt Lin <hunt.lin@tes-tec.com>
2023-03-13 17:18:37 +0800

    commit

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 bundle install
 bundle update
 
-bundle exec rake zeitwerk:check --trace
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake redmine:plugins RAILS_ENV=production
+bundle exec rake zeitwerk:check --trace
 
 sudo service apache2 restart
