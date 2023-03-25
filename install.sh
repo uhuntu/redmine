4195a270235e9bed99b27894954e95ae04772234
Hunt Redmine <hunt@red-mine.com>
2023-03-25 08:57:53 +0800

    commit

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 bundle install
 bundle update
 
-bundle exec rake zeitwerk:check --trace
+bundle exec rake zeitwerk:check --trace RAILS_ENV=production
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake redmine:plugins RAILS_ENV=production
 
