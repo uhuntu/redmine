7716b83ee05debac378b442ca5adb10b3cd0eb9d
Hunt Redmine <hunt@red-mine.com>
2023-03-27 09:28:33 +0800

    commit

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ bundle exec rake redmine:plugins RAILS_ENV=production
 
 bundle exec rake zeitwerk:check --trace RAILS_ENV=production
 
-#bundle exec rake full_text_search:synchronize RAILS_ENV=production
+bundle exec rake full_text_search:synchronize RAILS_ENV=production
 
 sudo service apache2 restart
